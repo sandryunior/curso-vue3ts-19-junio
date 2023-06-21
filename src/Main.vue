e040d3a367ffa97a529b017aa0ecba9dde0a46a8
sandryunior <sandra.morales3@hotmail.com>
2023-06-21 16:55:57 +0200

    tema 05-comunicacion-padre-hijo

--- src/Main.vue
+++ src/Main.vue
@@ -1,18 +1,25 @@
 <template>
-    <div>
-        <ContadorComposable/>
-        <ListaTareas/>
-    </div>
+        <BotonOnOff :active="modoOscuro" :toggle="cambiaModo"/>
+        <TextoPersonalizado texto="Otro texto" :esOscuro="modoOscuro"/>
 </template>
 
 <script setup lang="ts">
+// //@ts-ignore
+// import ListaTareas from "@components/ListaTareas"
+// //@ts-ignore
+// import MiContador from "@components/MiContador"
+// //@ts-ignore
+// import ContadorComposable from "@components/ContadorComposable"
+import {ref} from 'vue';
 //@ts-ignore
-import ListaTareas from "@components/ListaTareas"
+import BotonOnOff from './components/BotonOnOff.vue';
 //@ts-ignore
-import MiContador from "@components/MiContador"
-//@ts-ignore
-import ContadorComposable from "@components/ContadorComposable"
-//Pantalla 1 - Lista de tareas
+import TextoPersonalizado from './components/TextoPersonalizado.vue';
+
+const modoOscuro = ref(true)
+const cambiaModo = () => {
+    modoOscuro.value = !modoOscuro.value
+}
 </script>
 
 <style scoped>
---
+++ src/components/TextoPersonalizado.vue
@@ -0,0 +1,22 @@
+<template>
+    <div :class="esOscuro ? 'oscuro' : 'claro'">
+        <h2>{{ texto }}</h2>
+    </div>
+</template>
+<script setup lang="ts">
+defineProps<{
+    texto:string
+    esOscuro:boolean
+}>()
+</script>
+
+<style scoped>
+.oscuro{
+    background-color: transparent;
+    color: white;
+}
+.claro{
+    background-color: white;
+    color: black;
+}
+</style>
